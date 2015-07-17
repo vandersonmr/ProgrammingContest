100b08352eb5a6071a73bca79f78978692f01a32
vandersonmr <vandersonmr2@gmail.com>
2015-07-16 22:38:35 -0300

    PD no C

--- ContestUFFS/C.cpp
+++ ContestUFFS/C.cpp
@@ -4,9 +4,9 @@
 
 #define ll unsigned long long int
 
-long long degree(long long a, long long k, long long p) {
-  long long res = 1;
-  long long cur = a;
+ll degree(ll a, ll k, ll p) {
+  ll res = 1;
+  ll cur = a;
 
   while (k) {
     if (k%2) {
@@ -18,10 +18,10 @@ long long degree(long long a, long long k, long long p) {
   return res;
 }
 
-int get_degree(long long n, long long p) { // returns the degree with which p is in n!
+int get_degree(ll n, ll p) { // returns the degree with which p is in n!
   int degree_num = 0;
-  long long u = p;
-  long long temp = n;
+  ll u = p;
+  ll temp = n;
   while (u <= temp) {
     degree_num += temp/u;
     u *= p;
@@ -29,23 +29,23 @@ int get_degree(long long n, long long p) { // returns the degree with which p is
   return degree_num;
 }
 
-long long combinations(int n, int k, long long p) {
+ll combinations(int n, int k, ll p) {
   int num_degree = get_degree(n,p) - get_degree(n- k,p);
   int den_degree = get_degree(k,p);
   if (num_degree > den_degree) {
     return 0;
   }
-  long long res = 1;
-  for (long long i = n; i > n- k; --i) {
-    long long ti = i;
+  ll res = 1;
+  for (ll i = n; i > n- k; --i) {
+    ll ti = i;
     while(ti % p == 0) {
       ti /= p;
     }
     res = (res *ti)%p;
   }
-  long long denom = 1;
-  for (long long i = 1; i <= k; ++i) {
-    long long ti = i;
+  ll denom = 1;
+  for (ll i = 1; i <= k; ++i) {
+    ll ti = i;
     while(ti % p == 0) {
       ti /= p;
     }
@@ -69,30 +69,26 @@ ll calc_bin(ll n, ll k) {
   return r;
 }
 
+#define ac(i, j) (i == 0 || j == 0 ? 0 : w[i][j])
+
 int main () {
   int a, b;
   scanf("%d %d\n", &a, &b);
   char map[a+1][b+1];
+  int w[a+1][b+1];
   for (int i = 0; i < a; i++) {
     char c;
     for (int j = 0; j < b; j++) {
       scanf("%c", &c);
       map[i+1][j+1] = c;
+      w[i+1][j+1] = ac(i, j+1) + ac(i+1, j) + (c == '#'? 1 : 0) - ac(i, j);
     }
     scanf("\n");
   }
   int xa, ya, xb, yb;
   while (scanf("%d %d %d %d\n", &xa, &ya, &xb, &yb) != EOF) {
     int n = ((xb - xa)+1) * ((yb - ya)+1);
-    int z = 0;
-    for (int i = xa; i <= xb; i++) {
-      for (int j = ya; j <= yb; j++) {
-        if (map[i][j] == '#') {
-          ++z;
-        }
-      }
-    }
-    // n!/(z!(n-z)!)
+    int z = ac(xb, yb) - ac(xb, ya - 1) - ac(xa - 1, yb) + ac(xa-1, ya-1);
     printf("%llu\n", (combinations(n, z, 1000000007) ) - 1);
   }
   return 0;
